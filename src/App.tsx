f7073b7b4b64f93d46a1c5cdaf8d00dd105bf178
Gusales <dev.gussales@gmail.com>
2024-07-31 20:53:36 -0300

    feat: home page structured

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,110 @@
+import { Coffee, MapPin, Package, ShoppingCart, Timer } from '@phosphor-icons/react'
+
+import Logo from './assets/logo.svg'
+import CoffeeCoup from './assets/coffee-coup.png'
+import Placeholder from './assets/placeholder.jpg'
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <header>
+        <figure>
+          <img src={Logo} alt="Logotipo da Coffee Delivery" />
+        </figure>
+
+        <div>
+          <div>
+            <MapPin weight="fill" />
+            <p>Carapicuíba, SP</p>
+          </div>
+
+          <button>
+            <ShoppingCart weight="fill" />
+          </button>
+        </div>
+      </header>
+
+      <section>
+        <div>
+          <h1>
+            Encontre o café perfeito para qualquer hora do dia
+          </h1>
+          <h2>
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora
+          </h2>
+
+          <div>
+            <div>
+              <span>
+                <ShoppingCart />
+              </span>
+
+              <p>Compra simples e segura</p>
+            </div>
+
+            <div>
+              <span>
+                <Package weight="fill" />
+              </span>
+
+              <p>Embalagem mantém o café intacto</p>
+            </div>
+
+            <div>
+              <span>
+                <Timer weight="fill" />
+              </span>
+
+              <p>Entrega rápida e rastreada</p>
+            </div>
+
+            <div>
+              <span>
+                <Coffee weight="fill" />
+              </span>
+
+              <p>O café chega fresquinho até você</p>
+            </div>
+          </div>
+        </div>
+        <figure>
+          <img src={CoffeeCoup} alt="Imagem de um copo de café que está em volta das sementes e ramos de café" />
+        </figure>
+      </section>
+
+      <main>
+        <h2>Nossos cafés</h2>
+
+        <section>
+          <article>
+            <figure>
+              <img src={Placeholder} alt="" />
+            </figure>
+
+            <span>
+              tradicional
+            </span>
+
+            <p>Expresso Tradicional</p>
+
+            <p>O tradicional café feito com água quente e grãos moídos</p>
+
+            <div>
+              <div>
+                <p>R$</p>
+                <p>9,90</p>
+              </div>
+
+              <div>
+                <input type="number" name="" id="" />
+                <button>
+                  <ShoppingCart weight="fill" />
+                </button>
+              </div>
+            </div>
+          </article>
+        </section>
+      </main>
+    </>
   )
 }
