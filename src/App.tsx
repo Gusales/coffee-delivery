fc592db9b18847bfb1c803d1e86de249ee2c323e
Gusales <dev.gussales@gmail.com>
2024-08-03 20:58:07 -0300

    refactor: fixing layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { Coffee, Package, ShoppingCart, Timer } from '@phosphor-icons/react'
+import { ShoppingCart } from '@phosphor-icons/react'
 
-import CoffeeCoup from './assets/coffee-coup.png'
 import Placeholder from './assets/placeholder.jpg'
 import { Header } from './components/header'
 
@@ -8,54 +7,6 @@ export function App() {
   return (
     <div className="wrapper">
       <Header />
-
-      <section>
-        <div>
-          <h1>
-            Encontre o café perfeito para qualquer hora do dia
-          </h1>
-          <h2>
-            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora
-          </h2>
-
-          <div>
-            <div>
-              <span>
-                <ShoppingCart />
-              </span>
-
-              <p>Compra simples e segura</p>
-            </div>
-
-            <div>
-              <span>
-                <Package weight="fill" />
-              </span>
-
-              <p>Embalagem mantém o café intacto</p>
-            </div>
-
-            <div>
-              <span>
-                <Timer weight="fill" />
-              </span>
-
-              <p>Entrega rápida e rastreada</p>
-            </div>
-
-            <div>
-              <span>
-                <Coffee weight="fill" />
-              </span>
-
-              <p>O café chega fresquinho até você</p>
-            </div>
-          </div>
-        </div>
-        <figure>
-          <img src={CoffeeCoup} alt="Imagem de um copo de café que está em volta das sementes e ramos de café" />
-        </figure>
-      </section>
 
       <main>
         <h2>Nossos cafés</h2>
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,25 +5,27 @@ import './style.scss'
 
 export function Header(){
   return(
-    <header className="header__container">
-      <figure className="header__figure">
-        <img 
-          src={Logo} 
-          alt="Logotipo da Coffee Delivery" 
-          className="header__logo"
-        />
-      </figure>
+    <div className="container header__container">
+      <header>
+        <figure className="header__figure">
+          <img 
+            src={Logo} 
+            alt="Logotipo da Coffee Delivery" 
+            className="header__logo"
+          />
+        </figure>
 
-      <div className="header__actions">
-        <div className="header__location">
-          <MapPin weight="fill" className="header__location__icon" size={22} />
-          <p className="header__location__text">Carapicuíba, SP</p>
+        <div className="header__actions">
+          <div className="header__location">
+            <MapPin weight="fill" className="header__location__icon" size={22} />
+            <p className="header__location__text">Carapicuíba, SP</p>
+          </div>
+
+          <button className="header__cart" name="Acessar o seu carrinho">
+            <ShoppingCart weight="fill" className="header__cart__icon" size={22} />
+          </button>
         </div>
-
-        <button className="header__cart" name="Acessar o seu carrinho">
-          <ShoppingCart weight="fill" className="header__cart__icon" size={22} />
-        </button>
-      </div>
-    </header>
+      </header>
+    </div>
   )
 }
